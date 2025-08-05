2f09a31566540060b7fc2f1ac0f8caa7d4bd9bed
TCHAPCHA <kurilenkotimur@gmail.com>
2025-08-05 23:10:06 +0200

    Initial commit

--- DjangoBooks/settings.py
+++ DjangoBooks/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ycav!o$#3)x**v=bqg+wxowxr2!z)iz3$j92wm=e!dhp-z0+_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
